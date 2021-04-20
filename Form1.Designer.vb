a57ab8434706317fd6facd92368f1c710576119b
Afnan Fareez Haiqal Bin Alias <18DDT19F1091@student.ptss.edu.my>
2021-04-20 12:07:11 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.TxtBox1 = New System.Windows.Forms.TextBox()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.txtBox2 = New System.Windows.Forms.TextBox()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(95, 59)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "Button1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(302, 59)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(39, 13)
+        Me.lbl1.TabIndex = 1
+        Me.lbl1.Text = "Label1"
+        '
+        'TxtBox1
+        '
+        Me.TxtBox1.Location = New System.Drawing.Point(95, 155)
+        Me.TxtBox1.Name = "TxtBox1"
+        Me.TxtBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TxtBox1.TabIndex = 2
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(305, 151)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(75, 23)
+        Me.btn2.TabIndex = 3
+        Me.btn2.Text = "Button2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(499, 151)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(39, 13)
+        Me.lbl2.TabIndex = 4
+        Me.lbl2.Text = "Label2"
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(95, 248)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(75, 23)
+        Me.btn3.TabIndex = 5
+        Me.btn3.Text = "Button3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Location = New System.Drawing.Point(305, 257)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(39, 13)
+        Me.lbl3.TabIndex = 6
+        Me.lbl3.Text = "Label3"
+        '
+        'txtBox2
+        '
+        Me.txtBox2.Location = New System.Drawing.Point(95, 344)
+        Me.txtBox2.Name = "txtBox2"
+        Me.txtBox2.Size = New System.Drawing.Size(100, 20)
+        Me.txtBox2.TabIndex = 7
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(305, 340)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(75, 23)
+        Me.btn4.TabIndex = 8
+        Me.btn4.Text = "Button4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Location = New System.Drawing.Point(502, 350)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(39, 13)
+        Me.lbl4.TabIndex = 9
+        Me.lbl4.Text = "Label4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.txtBox2)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.TxtBox1)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents TxtBox1 As TextBox
+    Friend WithEvents btn2 As Button
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents btn3 As Button
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents txtBox2 As TextBox
+    Friend WithEvents btn4 As Button
+    Friend WithEvents lbl4 As Label
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Function Dan Sub")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Function Dan Sub")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7fe64de7-d170-413c-b1d7-4d61e482f479")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
