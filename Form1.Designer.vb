0940304dc0df2e84bb7c563051e169ad3ce8435b
Afnan Fareez Haiqal Bin Alias <18DDT19F1091@student.ptss.edu.my>
2021-04-20 13:02:57 +0800

    Contoh Function Dan Sub

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -124,7 +124,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(659, 399)
         Me.Controls.Add(Me.lbl4)
         Me.Controls.Add(Me.btn4)
         Me.Controls.Add(Me.txtBox2)
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,35 @@
 ﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Call Afnan()
+    End Sub
+    Private Sub Afnan()
+        lbl1.Text = "Data tersebut"
+    End Sub
 
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Call Fareez()
+    End Sub
+    Private Sub Fareez()
+        Dim intKira As String
+        intKira = Trim(TxtBox1.Text)
+        lbl2.Text = intKira
+    End Sub
+
+    Private Function btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        Call Haiqal()
+    End Function
+
+    Private Function Haiqal()
+        lbl3.Text = "Data Tersebut"
+        Return lbl3
+    End Function
+
+    Private Function btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        lbl4.Text = Nama()
+    End Function
+    Private Function Nama()
+        Dim strNama As String
+        strNama = txtBox2.Text
+        Return strNama
+    End Function
 End Class
