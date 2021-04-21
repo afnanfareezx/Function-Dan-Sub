34b96b37c315ddcf4ae9cd16d2a6ffbb2a8813f5
Afnan Fareez Haiqal Bin Alias <18DDT19F1091@student.ptss.edu.my>
2021-04-21 11:23:28 +0800

    Contoh Function And Sub

--- Form1.vb
+++ Form1.vb
@@ -7,13 +7,11 @@
     End Sub
 
     Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
-        Dim strTxt As String
-        strTxt = TxtBox1.Text
-        lbl2.Text = strTxt
-        Fareez(strFareez:=TxtBox1.Text)
+        Fareez(TxtBox1.Text)
     End Sub
     Private Sub Fareez(ByVal strFareez As String)
-        Dim strInput As String
+        strFareez = TxtBox1.Text
+        lbl2.Text = strFareez
     End Sub
 
     Private Function btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
@@ -22,17 +20,16 @@
 
     Private Function Haiqal()
         lbl3.Text = "Selamat Pagi"
-        Return lbl3.Text
+        Return Haiqal
     End Function
 
     Private Function btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
-        Dim strTxt2 As String
-        strTxt2 = txtBox2.Text
-        lbl4.Text = strTxt2
         Nama(strNama:=txtBox2.Text)
-        Return lbl4
     End Function
     Private Function Nama(ByVal strNama As String)
-        Dim str As String
+        Dim intShowUmur As Integer
+        intShowUmur = txtBox2.Text
+        lbl4.Text = intShowUmur
+        Return Nama
     End Function
 End Class
